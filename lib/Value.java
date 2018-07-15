8e123456d7d0bea6dfdfb76eb68a4fd06de559ff
Shawn Nigel Rebello <shawn.nigel@gmail.com>
2018-07-15 16:41:07 +0530

    Added default type & value of variable

--- lib/Value.java
+++ lib/Value.java
@@ -1,13 +1,17 @@
 package com.shawnrebello.baritsu.lib;
 
 public class Value {
-   String type = "UNDEFINED";
+   String type ;
    Object value;
 
    public Value (String type, Object value) {
      this.type = type;
      this.value = value;
    }
+   public Value () {
+    this.type = "UNDEFINED";
+    this.value = null;
+  }
 
    public String getType() {
      return this.type;
