42b945096a413d2d6fff90141fa21464f810a13e
Shawn Nigel Rebello <shawn.nigel@gmail.com>
2018-08-05 02:36:41 +0530

    Renamed recognizer

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ANTLR_PATH='/usr/local/lib/antlr-4.7.1-complete.jar'
 echo "Generating grammar files"
-cd grammar; java -jar $ANTLR_PATH -long-messages -no-listener -visitor *.g4 -o ../antlr; cd ..
+cd grammar; java -jar $ANTLR_PATH -long-messages -no-listener -visitor *.g4 -o ../recognizer; cd ..
 echo "Compiling source files"
 javac  */*.java */*.java -g -d .
 #alias baritsu='java com.shawnrebello.baritsu.src.Compiler'
